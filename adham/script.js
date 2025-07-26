bc47eca93c4e7fb4385512ea4104215caf73f18d
ad-ha-m <ad772009@gmail.com>
2025-07-27 01:18:22 +0300

    upgrade  by adham

---
+++ adham/script.js
@@ -0,0 +1,21 @@
+
+var name=prompt("enter a name")
+
+var num=prompt("enter a number")
+result=num%2
+if(result==0){
+console.log("even");
+
+}
+
+else if(result==1){
+    console.log("odd");
+    
+}
+
+else{
+    console.log("not a number");
+    
+}
+
+var degree=prompt("enter degree")
