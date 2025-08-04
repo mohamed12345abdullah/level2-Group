edd3eb1695d8c05de8e1cf66b2232f144b3e71f5
ad-ha-m <ad772009@gmail.com>
2025-08-04 16:45:47 +0300

    push

--- scope js/script.js
+++ scope js/script.js
@@ -14,31 +14,66 @@ console.log(x);
 //     .then(res => res.json())
 //     .then(data => console.log(data));
 
-var container = document.getElementById('container');
+
+
+
+
+
+
+function updateProduct(id){
+
+
+    let product = products.find(product => product._id === id);
+    console.log(product);
+    document.getElementById('puloadForm').name.value = product.name;
+    document.getElementById('puloadForm').price.value = product.price;
+    document.getElementById('puloadForm').description.value = product.description;
+    document.getElementById('puloadForm').image.value = product.image;
+    document.getElementById('puloadForm').submit.value = 'Update';
+
+}
+
+let products=[];
+var container = document.getElementById('list');
 console.log(container);
 async function fetchData() {
+    container.innerHTML = '';
     var result = await fetch('https://asyut-eng-synd.vercel.app/api/products')
     var response = await result.json();
     console.log(result);
     console.log(response);
-    let products = response.data;
+    products = response.data;
     console.log(products);
    
-    let html = '';
+    let html = `            
+            <div  class="product">
+                <img src="" alt="">
+                <h2 class="name"></h2>
+                <p class="price"></p>
+                <p class="description"></p>
+                <button class="buy">Buy</button>
+            </div>
+    
+    `;
     products.forEach(product => {
         html += `
 
-        <div id="product">
-                <img src="${product.image}" alt="">
-                <h2>${product.name}</h2>
-                <p>${product.price}</p>
-                <p>${product.description}</p>
-                <button>Buy</button>
-            </div>
+        <div id="row">
+        
+            <p>${product._id}</p>
+            <p>${product.name}</p>
+            <p>${product.price}</p>
+            <p>${product.description}</p>
+            <p>${product.name}</p> 
+            <p>${product.category}</p>
+            <button onclick="updateProduct('${product._id}')">update</button>
+            <button onclick="deleteProduct('${product._id}')">delete</button>
+     
+        </div>
         
         `;
     });
-    container.innerHTML = html; 
+    container.innerHTML += html; 
     if (result.ok) {
         console.log('success');
     }
@@ -49,9 +84,37 @@ async function fetchData() {
     console.log(response);
 
 }
+
+
+function cleanForm(){
+    document.getElementById('puloadForm').reset();
+}
 // form js
 fetchData();
-let form = document.getElementById('form');
+
+
+function addAndUpdateProduct(product,id){
+    fetch(`https://asyut-eng-synd.vercel.app/api/products/${id}`, {
+        method: id?'PUT':'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(product)
+
+    }).then(function(response){
+        
+        let res= response.json();
+        console.log(res);
+        fetchData();
+        return res
+    }).then(function(data){
+        let messageDiv = document.getElementById('message');
+        messageDiv.innerHTML = data.message;
+        console.log(data);
+        cleanForm();
+    })
+}
+let form = document.getElementById('puloadForm');
 console.log(form);
 form.addEventListener('submit', function(e){
     console.log('submit');
@@ -67,19 +130,27 @@ form.addEventListener('submit', function(e){
         image: image
     }
     let stringData = JSON.stringify(product);
-    fetch('https://asyut-eng-synd.vercel.app/api/products', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: stringData
-
-    }).then(function(response){
-        return response.json();
-    }).then(function(data){
-        console.log(data);
-    })
+    addAndUpdateProduct(product,id);
 
    
     console.log(name, price, description, image);
 }) 
+
+
+
+
+function deleteProduct(id){
+    console.log(id);
+    fetch(`https://asyut-eng-synd.vercel.app/api/products/${id}`, {
+        method: 'DELETE'
+    }).then(function(response){
+        let res= response.json();
+        console.log(res);
+        return res
+    }).then(function(data){
+        let messageDiv = document.getElementById('message');
+        messageDiv.innerHTML = data.message;
+        console.log(data);
+        fetchData();
+    })
+}
